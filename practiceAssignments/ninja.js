4dbec87468d4dfac3d469803f22492358395ebb2
RobertWingard <RobertWingard@Icloud.com>
2022-10-25 11:13:17 -0700

    ninjas

--- practiceAssignments/ninja.js
+++ practiceAssignments/ninja.js
@@ -1,3 +1,29 @@
-const ninja1 = new Ninja("Hyabusa");
+class Ninja{
+    constructor(name, health, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    
+    sayName(){
+        console.log(this.name);
+    }
+    showStats(){
+        console.log("health " + this.health , "speed " + this.speed , "str "  + this.strength)
+    }
+
+    drinkSake(){
+        this.health +=10;
+        console.log("updated health " + this.health)
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 100);
 ninja1.sayName();
+
 ninja1.showStats();
+
+ninja1.drinkSake();
+
+
