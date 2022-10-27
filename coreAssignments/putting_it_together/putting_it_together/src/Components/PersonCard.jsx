baf96f13b22cbf557db0159b669c812b4d9c7aa1
RobertWingard <RobertWingard@Icloud.com>
2022-10-26 20:57:52 -0700

    birthday

---
+++ coreAssignments/putting_it_together/putting_it_together/src/Components/PersonCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+class PersonCard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    update = () => {
+        let age = this.state.age + 1;
+        this.setState({age: age});
+    }
+
+
+    render(){
+        return (
+            <>
+            <div>
+            <h1>{this.props.lastName}, {this.props.firstName} </h1> 
+            <h3>Age: {this.state.age}</h3> 
+            <h3>Hair Color: {this.props.hairColor}</h3> 
+            </div>
+
+            <button onClick={this.update}> Increase { this.props.firstName } { this.props.lastName } </button>
+        </>
+        )
+    }
+}
+
+export default PersonCard;
